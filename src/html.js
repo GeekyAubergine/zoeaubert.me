76dc96656908c015ba4a236eaacaf8b4511fc76b
Zoe Aubert <hello@zoeaubert.me>
2023-01-06 08:18:40 +0000

    Try this

--- src/html.js
+++ src/html.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { graphql, useStaticQuery } from 'gatsby'
 
 export default function HTML(props) {
+    const { site } = useStaticQuery(
+        graphql`
+            query {
+                site {
+                    siteMetadata {
+                        title
+                        description
+                        author
+                        siteUrl
+                        image
+                    }
+                }
+            }
+        `,
+    )
+
+    const { title, description, author, siteUrl, image } = site.siteMetadata
+
     return (
         <html {...props.htmlAttributes}>
             <head>
@@ -12,11 +31,24 @@ export default function HTML(props) {
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
                 {props.headComponents}
+
+                <title>Zoe Aubert</title>
+                <meta name="title" content={title} />
+                <meta name="description" content={description} />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content={siteUrl} />
+                <meta property="og:title" content={title} />
+                <meta property="og:description" content={description} />
+                <meta property="og:image" content={image} />
+
+                <meta property="twitter:card" content="summary_large_image" />
+                <meta property="twitter:url" content={siteUrl} />
+                <meta property="twitter:title" content={title} />
+                <meta property="twitter:description" content={description} />
+                <meta property="twitter:image" content={image} />
             </head>
-            <body
-                className="hidden"
-                {...props.bodyAttributes}
-            >
+            <body className="hidden" {...props.bodyAttributes}>
                 {props.preBodyComponents}
                 <div
                     key={`body`}
