6d1ad7b8a7f513bb9ba8729e05995dd87a06d4a6
Zoe Aubert <hello@zoeaubert.me>
2023-01-09 07:06:53 +0000

    Remove title again

--- src/html.js
+++ src/html.js
@@ -5,7 +5,6 @@ export default function HTML(props) {
     return (
         <html {...props.htmlAttributes}>
             <head>
-                <title>Zoe Aubert</title>
                 <meta charSet="utf-8" />
                 <meta httpEquiv="x-ua-compatible" content="ie=edge" />
                 <meta
