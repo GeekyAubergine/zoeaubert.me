dd6f76599a1b1cb5ba30d8802f6ac7715cdc9e0c
Zoe Aubert <hello@zoeaubert.me>
2023-01-12 14:27:19 +0000

    Maybe it is needed

--- src/html.js
+++ src/html.js
@@ -37,7 +37,7 @@ export default function HTML(props) {
                     name="viewport"
                     content="width=device-width, initial-scale=1, shrink-to-fit=no"
                 />
-                {headComponents}
+                {props.headComponents}
             </head>
             <body className="hidden" {...props.bodyAttributes}>
                 {props.preBodyComponents}
