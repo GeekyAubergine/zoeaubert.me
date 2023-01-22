ad2201b5dff0210cab23912e958b74a07caff477
Zoe Aubert <hello@zoeaubert.me>
2023-01-22 14:31:30 +0000

    More little fixes

---
+++ src/_data/navlinks.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        name: 'MicroBlog',
+        target: '_blank',
+        rel: 'noopener',
+        url: 'https://geekyaubergine.com',
+    },
+    {
+        name: 'Blog',
+        target: '',
+        rel: '',
+        url: '/blog',
+    },
+    {
+        name: 'Photos',
+        target: '',
+        rel: '',
+        url: '/photos',
+    },
+    {
+        name: 'GitHub',
+        target: '_blank',
+        rel: 'noopener',
+        url: 'https://github.com/geekyaubergine',
+    },
+]
