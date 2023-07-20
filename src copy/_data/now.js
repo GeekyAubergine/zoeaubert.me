d749934043673b670d64062c3059e8f8097807fa
Zoe Aubert <hello@zoeaubert.me>
2023-07-20 01:01:40 +0100

    seems like all templates are acorss, now just to make it look pretty

--- src copy/_data/now.js
+++
@@ -1,9 +0,0 @@
-const config = require('../../config')
-
-module.exports = async function () {
-    const { apiUrl } = config
-
-    const request = await fetch(`${apiUrl}/now.txt`)
-
-    return request.text()
-}
--- src/_data/now.js
+++ src/_data/now.js
@@ -2,8 +2,8 @@ const config = require('../../config')
 
 module.exports = async function () {
     const { apiUrl } = config
-    
-    const request = await fetch(`${apiUrl}/links.txt`)
+
+    const request = await fetch(`${apiUrl}/now.md`)
 
     return request.text()
 }
