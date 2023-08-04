4e1ce24d7f82ba3e2be869057275e9219a36673a
Zoe Aubert <hello@zoeaubert.me>
2023-08-05 00:01:41 +0100

    pretty happy with that so far

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    darkMode: 'media',
+    darkMode: 'class',
     content: ['./src/**/**/*.{njk,md}'],
     safelist: [
         'gatsby-highlight',
@@ -25,19 +25,53 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                background: '#181818',
-                headings: '#E4E3E7',
-                text: '#EDEDED',
-                secondary: '#ABA9B0',
-                tag: '#181D27',
-                accent: '#FEB847', //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
-                code: {
-                    DEFAULT: '#181D27',
-                    text: '#0061DF8EA45C',
+                background: {
+                    DEFAULT: '#FFFFFF',
+                    dark: '#181818',
                 },
-                border: '#514F55',
-                middleGray: '#888888',
+                headings: {
+                    DEFAULT:'#080808',
+                    dark: '#EDEDED',
+                },
+                text: {
+                    DEFAULT:'#080808',
+                    dark: '#EDEDED',
+                },
+                secondary: {
+                    DEFAULT:'#55535A',
+                    dark: '#ABA9B0',
+                },
+                accent: {
+                    DEFAULT:'#842A87', //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
+                    dark: '#ED95E6',
+                },
+                code: {
+                    DEFAULT: '#D4D4D4',
+                    dark: "#424546",
+                },
+                border: {
+                    DEFAULT:'#55535A',
+                    dark: '#181818',
+                },
+                middleGray: {
+                    DEFAULT:'#888888',
+                    dark: '#181818',
+                },
             },
+            // colors: {
+            //     background: '#181818',
+            //     headings: '#E4E3E7',
+            //     text: '#EDEDED',
+            //     secondary: '#ABA9B0',
+            //     tag: '#181D27',
+            //     accent: "#FFA0F8" ,//'#FFA0F8', //"BC8DFE" //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
+            //     code: {
+            //         DEFAULT: '#181D27',
+            //         text: '#0061DF8EA45C',
+            //     },
+            //     border: '#514F55',
+            //     middleGray: '#888888',
+            // },
         },
         fontFamily: {
             sans: ['Helvetica', 'Arial', 'sans-serif'],
