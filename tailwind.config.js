6e2a1ad4a1786f502e7471bdd78401a091cedb3f
Zoe Aubert <hello@zoeaubert.me>
2023-08-09 23:50:24 +0100

    mobile view looking alright

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,52 +30,48 @@ module.exports = {
                     dark: '#181818',
                 },
                 headings: {
-                    DEFAULT:'#080808',
+                    DEFAULT: '#080808',
                     dark: '#EDEDED',
                 },
                 text: {
-                    DEFAULT:'#080808',
+                    DEFAULT: '#080808',
                     dark: '#EDEDED',
                 },
                 secondary: {
-                    DEFAULT:'#55535A',
+                    DEFAULT: '#5A585F',
                     dark: '#ABA9B0',
                 },
                 accent: {
-                    DEFAULT:'#842A87', //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
+                    DEFAULT: '#842A87', //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
                     dark: '#ED95E6',
                 },
                 code: {
                     DEFAULT: '#D4D4D4',
-                    dark: "#424546",
+                    dark: '#303030',
                 },
                 border: {
-                    DEFAULT:'#55535A',
-                    dark: '#181818',
+                    DEFAULT: '#AAAAAA',
+                    dark: '#55535A',
                 },
                 middleGray: {
-                    DEFAULT:'#888888',
+                    DEFAULT: '#888888',
                     dark: '#181818',
                 },
             },
-            // colors: {
-            //     background: '#181818',
-            //     headings: '#E4E3E7',
-            //     text: '#EDEDED',
-            //     secondary: '#ABA9B0',
-            //     tag: '#181D27',
-            //     accent: "#FFA0F8" ,//'#FFA0F8', //"BC8DFE" //'#FEB847',//#F58123', //'#89BA6A', //'#FEB847', //'#DEB9FF', //'#BB9EE0',//'#FEB847', ///#FEB847",//"#F2DE7C",//"#FEB847",//D9BBFF//00D5C6
-            //     code: {
-            //         DEFAULT: '#181D27',
-            //         text: '#0061DF8EA45C',
-            //     },
-            //     border: '#514F55',
-            //     middleGray: '#888888',
-            // },
+            typography: ({ theme }) => ({
+                zoe: {
+                    css: {
+                        '--tw-prose-quote-borders': theme('colors.accent.DEFAULT'),
+                        '--tw-prose-invert-quote-borders': theme('colors.accent.dark'),
+                        // '--tw-prose-code': theme('colors.red[900]'),
+                        // '--tw-prose-pre-code': theme('colors.blue[200]'),
+                    },
+                },
+            }),
         },
         fontFamily: {
             sans: ['Helvetica', 'Arial', 'sans-serif'],
         },
     },
-    plugins: [],
+    plugins: [require('@tailwindcss/typography')],
 }
