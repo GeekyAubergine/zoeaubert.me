afac7b461a0ec3e3b16a4a16e520cdb929f0d223
Zoe Aubert <hello@zoeaubert.me>
2025-11-12 23:04:39 +0000

    fix tests

--- src/domain/models/review/book_review.rs
+++ src/domain/models/review/book_review.rs
@@ -21,6 +21,13 @@ const REGEX_MICRO: Lazy<Regex> =
 const REGEX_LEGACY_STYLE_AUTHOR_TITLE: Lazy<Regex> =
     Lazy::new(|| Regex::new(r"\[(.*)\].*by (.*) 📚").unwrap());
 
+struct ExtractedReview {
+    pub title: String,
+    pub author: String,
+    pub score: u8,
+    pub review: String,
+}
+
 #[derive(Debug, Clone)]
 pub struct BookReview {
     pub title: String,
@@ -32,15 +39,39 @@ pub struct BookReview {
 
 impl BookReview {
     pub fn from_micropost(post: MicroPost) -> Result<BookReview> {
-        parse_markdown_into_book_review(ReviewSource::MicroPost(post.clone()), &post.content)
+        let review = parse_markdown_into_book_review(&post.content)?;
+
+        Ok(BookReview {
+            title: review.title,
+            author: review.author,
+            score: review.score,
+            review: review.review,
+            source: ReviewSource::MicroPost(post),
+        })
     }
 
     pub fn from_mastodon_post(post: MastodonPost) -> Result<BookReview> {
-        parse_markdown_into_book_review(ReviewSource::MastodonPost(post.clone()), &post.content())
+        let review = parse_markdown_into_book_review(&post.content())?;
+
+        Ok(BookReview {
+            title: review.title,
+            author: review.author,
+            score: review.score,
+            review: review.review,
+            source: ReviewSource::MastodonPost(post),
+        })
     }
 
     pub fn from_review_source(source: ReviewSource) -> Result<BookReview> {
-        parse_markdown_into_book_review(source.clone(), source.content())
+        let review = parse_markdown_into_book_review(&source.content())?;
+
+        Ok(BookReview {
+            title: review.title,
+            author: review.author,
+            score: review.score,
+            review: review.review,
+            source,
+        })
     }
 }
 
@@ -58,19 +89,19 @@ impl BookReviews {
     }
 }
 
-fn parse_markdown_into_book_review(source: ReviewSource, content: &str) -> Result<BookReview> {
+fn parse_markdown_into_book_review(content: &str) -> Result<ExtractedReview> {
     if (content.starts_with("Finished") || content.starts_with("I finished")) {
-        return parse_legacy_post(source, content);
+        return parse_legacy_post(content);
     }
 
     if (content.starts_with("<p>")) {
-        return parse_mastodon_modern_post(source, content);
+        return parse_mastodon_modern_post(content);
     }
 
-    parse_new_style(source, content)
+    parse_new_style(content)
 }
 
-fn parse_legacy_post(source: ReviewSource, content: &str) -> Result<BookReview> {
+fn parse_legacy_post(content: &str) -> Result<ExtractedReview> {
     let content = content.replace("\\n", "\n");
 
     let lines = content.lines().collect::<Vec<&str>>();
@@ -95,12 +126,11 @@ fn parse_legacy_post(source: ReviewSource, content: &str) -> Result<BookReview> 
             second_line_split.get(1),
         ) {
             let score = score.trim().parse().unwrap();
-            return Ok(BookReview {
+            return Ok(ExtractedReview {
                 title: title.as_str().to_string(),
                 author: author.as_str().to_string(),
                 score,
                 review: review.trim().to_string(),
-                source,
             });
         }
     }
@@ -108,16 +138,16 @@ fn parse_legacy_post(source: ReviewSource, content: &str) -> Result<BookReview> 
     Err(BookError::unable_to_parse_book(content.to_string()))
 }
 
-fn parse_mastodon_modern_post(source: ReviewSource, content: &str) -> Result<BookReview> {
+fn parse_mastodon_modern_post(content: &str) -> Result<ExtractedReview> {
     let content = content
         .replace("</p><p>", "\n")
         .replace("<p>", "")
         .replace("</p>", "");
 
-    parse_new_style(source, &content)
+    parse_new_style(&content)
 }
 
-fn parse_new_style(source: ReviewSource, content: &str) -> Result<BookReview> {
+fn parse_new_style(content: &str) -> Result<ExtractedReview> {
     let content = content.replace("\\n", "\n");
 
     let lines = content.lines().collect::<Vec<&str>>();
@@ -147,12 +177,11 @@ fn parse_new_style(source: ReviewSource, content: &str) -> Result<BookReview> {
             second_line_split.get(1),
         ) {
             let score = score.trim().parse().unwrap();
-            return Ok(BookReview {
+            return Ok(ExtractedReview {
                 title: title.trim().to_string(),
                 author: author.trim().to_string(),
                 score,
                 review: review.trim().to_string(),
-                source,
             });
         }
     }
@@ -160,66 +189,66 @@ fn parse_new_style(source: ReviewSource, content: &str) -> Result<BookReview> {
     Err(BookError::unable_to_parse_book(content.to_string()))
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-//     #[test]
-//     fn test_parse_mastodon_modern_post() {
-//         let content = r#"<p>Legion by Dan Abnett</p><p>2/5 - The story is good, but I found it hard to follow in places.</p>"#;
+    #[test]
+    fn test_parse_mastodon_modern_post() {
+        let content = r#"<p>Legion by Dan Abnett</p><p>2/5 - The story is good, but I found it hard to follow in places.</p>"#;
 
-//         let review = parse_markdown_into_book_review(content).unwrap();
+        let review = parse_markdown_into_book_review(content).unwrap();
 
-//         assert_eq!(review.title, "Legion");
-//         assert_eq!(review.author, "Dan Abnett");
-//         assert_eq!(review.score, 2);
-//         assert_eq!(
-//             review.review,
-//             "The story is good, but I found it hard to follow in places."
-//         );
-//     }
+        assert_eq!(review.title, "Legion");
+        assert_eq!(review.author, "Dan Abnett");
+        assert_eq!(review.score, 2);
+        assert_eq!(
+            review.review,
+            "The story is good, but I found it hard to follow in places."
+        );
+    }
 
-//     #[test]
-//     fn test_parse_markdown_modern_post() {
-//         let content = r#"The First Heretic by Aaron Dembski-Bowden
+    #[test]
+    fn test_parse_markdown_modern_post() {
+        let content = r#"The First Heretic by Aaron Dembski-Bowden
 
-//         4/5 - Incredible. I'm looking forward to reading this again"#;
+        4/5 - Incredible. I'm looking forward to reading this again"#;
 
-//         let review = parse_markdown_into_book_review(content).unwrap();
+        let review = parse_markdown_into_book_review(content).unwrap();
 
-//         assert_eq!(review.title, "The First Heretic");
-//         assert_eq!(review.author, "Aaron Dembski-Bowden");
-//         assert_eq!(review.score, 4);
-//         assert_eq!(
-//             review.review,
-//             "Incredible. I'm looking forward to reading this again"
-//         );
-//     }
+        assert_eq!(review.title, "The First Heretic");
+        assert_eq!(review.author, "Aaron Dembski-Bowden");
+        assert_eq!(review.score, 4);
+        assert_eq!(
+            review.review,
+            "Incredible. I'm looking forward to reading this again"
+        );
+    }
 
-//     #[test]
-//     fn test_parse_markdown_modern_post_with_no_review() {
-//         let content = r#"\nFulgrim by Graham McNeill\n\n4/5"#;
+    #[test]
+    fn test_parse_markdown_modern_post_with_no_review() {
+        let content = r#"\nFulgrim by Graham McNeill\n\n4/5"#;
 
-//         let review = parse_markdown_into_book_review(content).unwrap();
+        let review = parse_markdown_into_book_review(content).unwrap();
 
-//         assert_eq!(review.title, "Fulgrim");
-//         assert_eq!(review.author, "Graham McNeill");
-//         assert_eq!(review.score, 4);
-//         assert_eq!(review.review, "");
-//     }
+        assert_eq!(review.title, "Fulgrim");
+        assert_eq!(review.author, "Graham McNeill");
+        assert_eq!(review.score, 4);
+        assert_eq!(review.review, "");
+    }
 
-//     #[test]
-//     fn test_parse_microblog_legacy_post() {
-//         let content = r#"Finished reading: [The Devastation of Baal](https://oku.club/book/the-devastation-of-baal-by-guy-haley-w22tr) by Guy Haley 📚\n\n4/5 - Excellent book, the descriptions of both the [Blood Angles](https://warhammer40k.fandom.com/wiki/Blood_Angels) and [Tryranids](https://warhammer40k.fandom.com/wiki/Tyranids) are amazing. I would highly recommend it to anyone who's a fan of either faction (or space marines and Warhammer in general).\n"#;
+    #[test]
+    fn test_parse_microblog_legacy_post() {
+        let content = r#"Finished reading: [The Devastation of Baal](https://oku.club/book/the-devastation-of-baal-by-guy-haley-w22tr) by Guy Haley 📚\n\n4/5 - Excellent book, the descriptions of both the [Blood Angles](https://warhammer40k.fandom.com/wiki/Blood_Angels) and [Tryranids](https://warhammer40k.fandom.com/wiki/Tyranids) are amazing. I would highly recommend it to anyone who's a fan of either faction (or space marines and Warhammer in general).\n"#;
 
-//         let review = parse_markdown_into_book_review(content).unwrap();
+        let review = parse_markdown_into_book_review(content).unwrap();
 
-//         assert_eq!(review.title, "The Devastation of Baal");
-//         assert_eq!(review.author, "Guy Haley");
-//         assert_eq!(review.score, 4);
-//         assert_eq!(
-//             review.review,
-//             "Excellent book, the descriptions of both the [Blood Angles](https://warhammer40k.fandom.com/wiki/Blood_Angels) and [Tryranids](https://warhammer40k.fandom.com/wiki/Tyranids) are amazing. I would highly recommend it to anyone who's a fan of either faction (or space marines and Warhammer in general)."
-//         );
-//     }
-// }
+        assert_eq!(review.title, "The Devastation of Baal");
+        assert_eq!(review.author, "Guy Haley");
+        assert_eq!(review.score, 4);
+        assert_eq!(
+            review.review,
+            "Excellent book, the descriptions of both the [Blood Angles](https://warhammer40k.fandom.com/wiki/Blood_Angels) and [Tryranids](https://warhammer40k.fandom.com/wiki/Tyranids) are amazing. I would highly recommend it to anyone who's a fan of either faction (or space marines and Warhammer in general)."
+        );
+    }
+}
